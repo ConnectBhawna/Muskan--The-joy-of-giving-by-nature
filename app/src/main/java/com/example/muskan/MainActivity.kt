9f5d1381b3583684992e98e97eedfaf6bb4c96f6
Home <sara>
2022-08-05 14:14:42 +0530

    - Added both light and dark splash.xml (Splash Screens). - Added avd_anim.xml and avd_anim_night.xml Animated Vector drawables. - Edited AndroidManifest.xml to support Splash Screen theme. - Upgraded min sdk from 19 to 21 for splash screen support.

--- app/src/main/java/com/example/muskan/MainActivity.kt
+++ app/src/main/java/com/example/muskan/MainActivity.kt
@@ -2,9 +2,23 @@ package com.example.muskan
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen().apply {
+
+            /**
+             * Simulating a delay, in production app it could be a network call
+             * or setting up all resources before setting the content view the activity
+             * */
+            setKeepOnScreenCondition {
+                try {
+                    Thread.sleep(2000L)
+                } catch (e: Exception) { println(e.stackTrace) }
+                false
+            }
+        }
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
