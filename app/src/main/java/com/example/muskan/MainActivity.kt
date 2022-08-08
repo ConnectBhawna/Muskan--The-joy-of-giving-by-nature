eeb53762c9d27fecef19a42d16012b9fa81d931b
CodeFlyAir <95049059+CodeFlyAir@users.noreply.github.com>
2022-08-08 17:57:13 +0530

    Added Sign In and Register screen UI designs

--- app/src/main/java/com/example/muskan/MainActivity.kt
+++ app/src/main/java/com/example/muskan/MainActivity.kt
@@ -4,21 +4,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen().apply {
 
-            /**
-             * Simulating a delay, in production app it could be a network call
-             * or setting up all resources before setting the content view the activity
-             * */
-            setKeepOnScreenCondition {
-                try {
-                    Thread.sleep(2000L)
-                } catch (e: Exception) { println(e.stackTrace) }
-                false
-            }
+        /**
+         * Simulating a delay, in production app it could be a network call
+         * or setting up all resources before setting the content view the activity
+         * */
+        setKeepOnScreenCondition {
+            try {
+                Thread.sleep(2000L)
+            } catch (e: Exception) { println(e.stackTrace) }
+            false
         }
+    }
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
