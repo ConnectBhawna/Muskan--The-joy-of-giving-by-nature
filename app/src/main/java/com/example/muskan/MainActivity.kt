d9a6f1e846493c0456dfe5ef11b7917ff93580f4
Hawk Itzme <smquasim016@gmail.com>
2022-08-21 23:40:35 +0530

    Added Action Bar

--- app/src/main/java/com/example/muskan/MainActivity.kt
+++ app/src/main/java/com/example/muskan/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.muskan
 
-import androidx.appcompat.app.AppCompatActivity
+import com.example.muskan.R
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 
@@ -9,7 +13,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen().apply {
 
-        /**
+        /**q
          * Simulating a delay, in production app it could be a network call
          * or setting up all resources before setting the content view the activity
          * */
@@ -22,5 +26,25 @@ class MainActivity : AppCompatActivity() {
     }
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //Adding an action bar
+        setSupportActionBar(findViewById(R.id.my_toolbar))
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(com.example.muskan.R.menu.menutoolbar, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_favorite -> Toast.makeText(this, "Favourite selected", Toast.LENGTH_SHORT)
+                .show()
+            R.id.action_settings -> Toast.makeText(this, "Settings selected", Toast.LENGTH_SHORT)
+                .show()
+            else -> {}
+        }
+
+        return true
     }
 }
