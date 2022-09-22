42ac838c8083669425bbde420ee655716ae719e5
aditya <aditya27872014@gami.com>
2022-09-22 10:17:11 +0530

    feature : Admin & User page

--- app/src/main/java/com/example/muskan/SignInActivity.java
+++ app/src/main/java/com/example/muskan/SignInActivity.java
@@ -8,10 +8,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class SignInActivity extends AppCompatActivity
 {
+    EditText email,password;
+    ImageButton img;
     
     @Override
     protected void onCreate (Bundle savedInstanceState)
@@ -22,6 +27,28 @@ public class SignInActivity extends AppCompatActivity
 //        actionbar.hide();
     
         test();
+        //login details check
+        email = findViewById(R.id.loginEmailId);
+        password = findViewById(R.id.loginPassword);
+        img = findViewById(R.id.loginButton);
+        img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SignInActivity.this,choiceActivity.class));
+                //for admin login
+//                if(email.getText().toString().equals("admin123@gmail.com")){
+//                    if(!password.getText().toString().equals("admin123")){
+//                        Toast.makeText(SignInActivity.this, "wrong password", Toast.LENGTH_SHORT).show();
+//                    }else{
+//                    startActivity(new Intent(SignInActivity.this ,adminActivity.class));
+//                }
+//                }
+//                else{
+//                    //for other users this will direct to firstpage
+//                    startActivity(new Intent(SignInActivity.this ,firstPage.class));
+//                }
+            }
+        });
     }
     
     /*
